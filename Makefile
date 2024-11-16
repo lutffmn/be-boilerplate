895300b24fb68c6fb7424020762ec61b8104ad80
lutffmn <lutffmn@gmail.com>
2024-11-16 14:44:42 +0700

    set up interfaces

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
-mod-init:
+# initialize go mod
+mod:
 	@ read -p "Module name: " Name; \
 	go mod init $${Name}
 
+# installing dependencies
 mod-install:
 	go get -u github.com/jackc/pgx/v5
 	go get -u github.com/spf13/viper
--- config/config.go
+++ config/config.go
@@ -1,1 +1,50 @@
 package config
+
+import (
+	"strings"
+	"sync"
+
+	"github.com/spf13/viper"
+)
+
+type (
+	Config struct {
+		Server *Server
+		DB     *DB
+	}
+
+	Server struct {
+		Port int
+	}
+
+	DB struct {
+		Host     string
+		Port     int
+		User     string
+		Password string
+		DBName   string
+		SSLMode  string
+		TimeZone string
+	}
+)
+
+var (
+	once           sync.Once
+	configInstance *Config
+)
+
+func GetConfig() *Config {
+	once.Do(func() {
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath("./")
+		viper.AutomaticEnv()
+		viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+		if err := viper.ReadInConfig(); err != nil {
+			panic(err)
+		}
+	})
+
+	return configInstance
+}
